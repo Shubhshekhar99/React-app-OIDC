7ffcdbf3f5070788950ade51c9f25eb2364b6b9d
Shubhshekhar99 <155132346+Shubhshekhar99@users.noreply.github.com>
2025-12-18 16:46:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from Python Frontend 🚀</h1>"
+    return "Hello!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000)
